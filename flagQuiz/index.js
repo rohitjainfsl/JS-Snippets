2b11edc664e9e832b372bf7c02ee2cba92c0c1ee
Rohit Jain <rohit@fullstacklearning.com>
2022-12-26 22:47:59 +0530

    flag quiz

--- flagQuiz/index.js
+++ flagQuiz/index.js
@@ -1,12 +1,43 @@
 import codesObject from './countryCodes.js'
 
-console.log(codesObject)
-
-getARandomCountry(Object.keys(codesObject))
-
 const alreadyExist = []
+let randomCountry = {}
+let randomOptions = []
 
-function getARandomCountry(arr){
-  const random = Math.floor(Math.random() * arr.length)
+alreadyExist.length <= 10 ? initialize() : done();
   
+
+function initialize() {
+  console.log(codesObject)
+  randomCountry = getARandomCountry(Object.keys(codesObject))
+
+  //putting the right value in the array
+  for (const [key, value] of Object.entries(randomCountry)) {
+    randomOptions.push(`${value}`);
+  }
+  console.log(randomOptions)
+
+  //putting
+
+  const flagSrc = `https://flagcdn.com/${Object.keys(randomCountry)}.svg`
+
+  document.querySelector(".question img").src = flagSrc
+  document.querySelector(".count").innerHTML = alreadyExist.length
+}
+
+
+function getARandomCountry(arr) {
+  randomCountry = {}
+  const random = Math.floor(Math.random() * arr.length)
+  if (alreadyExist.includes(random)) return getARandomCountry(arr)
+  else {
+    alreadyExist.push(random)
+    randomCountry[arr[random]] = codesObject[arr[random]]
+    return randomCountry
+  }
+}
+
+
+function done(){
+  console.log("DONE")
 }
