83019a6759ea1b1942b339f3f7d1b5614ad7d6c1
rohitjainfsl <rohit@fullstacklearning.com>
2023-03-23 19:48:03 +0530

    word puzzle program

---
+++ wordPuzzles/wordPuzzles.js
@@ -0,0 +1,60 @@
+import data from "./data.js";
+
+const puzzlesDiv = document.querySelector("#puzzles");
+let wordCounter = 0;
+
+// startCounter();
+
+for (let i = 0; i < data.length; i++) {
+  const jumbled = document.createElement("div");
+  jumbled.classList.add("jumbled");
+
+  const h2 = document.createElement("h2");
+  h2.innerHTML = getJumbledWord(wordCounter);
+
+  const hint = document.createElement("p");
+  hint.innerHTML = "HINT: " + data[wordCounter].hint;
+
+  jumbled.append(h2);
+  jumbled.append(hint);
+
+  const userAnswer = document.createElement("div");
+  userAnswer.classList.add("userAnswer");
+  const input = document.createElement("input");
+  input.type = "text";
+  userAnswer.append(input);
+
+  puzzlesDiv.append(jumbled);
+  puzzlesDiv.append(userAnswer);
+  wordCounter++;
+}
+
+function getJumbledWord(wordCounter) {
+  let existingChars = [];
+  let jumbledWord = "";
+  const wordArray = data[wordCounter].word.toUpperCase().split("");
+  for (let i = 0; i < wordArray.length; i++) {
+    jumbledWord += wordArray[getARandomValue(wordArray.length, existingChars)];
+  }
+  return jumbledWord;
+}
+
+function getARandomValue(len, existingChars) {
+  const val = Math.floor(Math.random() * len);
+  if (existingChars.includes(val)) return getARandomValue(len, existingChars);
+  else {
+    existingChars.push(val);
+    return val;
+  }
+}
+
+function startCounter() {
+  let limit = 30;
+  const counterDiv = document.querySelector(".counter");
+  counterDiv.innerHTML = limit;
+  setInterval(() => {
+    if (limit === 0)
+        document.querySelector("form").submit()
+    else counterDiv.innerHTML = --limit;
+  }, 1000);
+}
