c6d5be969803de37b289469c8b95e43d08fd90e2
rohitjainfsl <rohit@fullstacklearning.com>
2023-03-24 13:21:17 +0530

    added timer and displayed results

--- wordPuzzles/wordPuzzles.js
+++ wordPuzzles/wordPuzzles.js
@@ -1,9 +1,25 @@
-import data from "./data.js";
+const data = [
+  {
+    word: "Computer",
+    hint: "it can do calculations, play sounds and video. You can view the world's information on it",
+  },
+  {
+    word: "python",
+    hint: "It's an animal as well as a programming language",
+  },
+  {
+    word: "morning",
+    hint: "what comes after night",
+  },
+];
 
 const puzzlesDiv = document.querySelector("#puzzles");
+const form = document.querySelector("form");
 let wordCounter = 0;
+let userAnswers = [];
+let interval = ''
 
-// startCounter();
+startCounter();
 
 for (let i = 0; i < data.length; i++) {
   const jumbled = document.createElement("div");
@@ -52,9 +68,41 @@ function startCounter() {
   let limit = 30;
   const counterDiv = document.querySelector(".counter");
   counterDiv.innerHTML = limit;
-  setInterval(() => {
-    if (limit === 0)
-        document.querySelector("form").submit()
-    else counterDiv.innerHTML = --limit;
+  interval = setInterval(() => {
+    if (limit === 0) {
+      document.querySelector("form").submit();
+      submitAnswers();
+    } else counterDiv.innerHTML = --limit;
   }, 1000);
+}
+
+document.querySelector("form").onsubmit = (e) => {
+  e.preventDefault();
+  submitAnswers();
+};
+
+function submitAnswers() {
+  clearInterval(interval)
+  userAnswers = Array.from(form.elements);
+  userAnswers.pop();
+  revealAnswers();
+}
+
+function revealAnswers() {
+  form.style.display = "none";
+  document.querySelector("#results").style.display = "block";
+
+  for (let i = 0; i < data.length; i++) {
+    const div = document.createElement("div");
+    const correctAnswer = document.createElement("p");
+    correctAnswer.innerHTML = "Correct Answer: " + data[i].word.toUpperCase();
+
+    const yourAnswer = document.createElement("p");
+    yourAnswer.innerHTML = "Your Answer: " + userAnswers[i].value.toUpperCase();
+
+    div.append(correctAnswer);
+    div.append(yourAnswer);
+
+    document.querySelector("#results").append(div);
+  }
 }
