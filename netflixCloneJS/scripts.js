c58ac0bdf8422d0da7cf60af0eb852de2e38e9ef
Rohit Jain <rohit@fullstacklearning.com>
2023-09-20 08:42:26 +0530

    adding pagination

--- netflixCloneJS/scripts.js
+++ netflixCloneJS/scripts.js
@@ -1,10 +1,11 @@
-const API_KEY = YOUR_API_KEY_HERE;
+const API_KEY = "8125db8f67d23da1d30f6063b1b794b8";
 const img_base_path = "https://image.tmdb.org/t/p/original";
 const moviesDiv = document.querySelector("#movies .movie-wrapper");
 const genreList = document.querySelector(".genreList ul");
 const modal = document.querySelector(".modal");
 const iframeWrapper = document.querySelector(".iframe-wrapper");
 const closeModal = document.querySelector(".close-modal");
+const paging = document.querySelector(".paging");
 let movieResults = "";
 
 async function fetchGenres() {
@@ -17,12 +18,54 @@ async function fetchGenres() {
 }
 
 async function fetchTrendingMovies() {
-  const response = await fetch(
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=" +
-      API_KEY
-  );
+  // console.log(window.location)
+  const url = new URLSearchParams(window.location.href);
+  
+  // for (const p of url) {
+  //   console.log(p);
+  // }
+  let response;
+  if (url[0].includes("page") && Number(url[1]) !== "") {
+    response = await fetch(
+      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=" +
+        page +
+        "&sort_by=popularity.desc&api_key=" +
+        API_KEY
+    );
+  }
+  else{
+    response = await fetch(
+      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=" +
+        API_KEY
+    );
+  }
+  
   movieResults = await response.json();
+  if (paging.childElementCount === 0) {
+    movieResults.total_pages > 100
+      ? createPaging(100)
+      : createPaging(movieResults.total_pages);
+  }
+  // console.log(movieResults.results)
   showMovies(movieResults.results);
+}
+
+function createPaging(limit) {
+  for (let i = 1; i <= limit; i++) {
+    const pages = document.createElement("a");
+    pages.innerHTML = i;
+    pages.classList.add("page");
+    if (i === 1) pages.classList.add("active");
+    pages.href = "?page=" + i;
+    pages.setAttribute("target", "_parent");
+    pages.setAttribute("onclick", "nextPage(event, this)");
+    paging.append(pages);
+  }
+}
+
+function nextPage(event, elem) {
+  // event.preventDefault();
+  fetchTrendingMovies(elem.innerHTML);
 }
 
 function showGenres(data) {
@@ -83,9 +126,9 @@ async function watchTrailer(event, element) {
   iframeWrapper.append(iframe);
 }
 
-function closeTrailer(){
-  iframeWrapper.querySelector("iframe").src = ""
-  modal.style.display = "none"
+function closeTrailer() {
+  iframeWrapper.querySelector("iframe").src = "";
+  modal.style.display = "none";
 }
 
 function toggleSidebarStickyness() {
@@ -110,4 +153,4 @@ fetchGenres();
 fetchTrendingMovies();
 window.addEventListener("scroll", toggleSidebarStickyness);
 
-closeModal.addEventListener("click", closeTrailer)
+closeModal.addEventListener("click", closeTrailer);
