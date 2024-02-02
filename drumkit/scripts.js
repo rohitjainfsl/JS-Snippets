82d091cdc535f18e61f49f591cae77bc83200f60
Rohit Jain <rohit@fullstacklearning.com>
2024-02-02 12:58:49 +0530

    background color on keypress

--- drumkit/scripts.js
+++ drumkit/scripts.js
@@ -11,8 +11,11 @@ Object.values(instruments).forEach((instrument) => {
   const img = document.createElement("img");
   const audio = document.createElement("audio");
   box.classList.add("box");
+  box.classList.add(instrument);
 
   img.src = "assets/" + instrument + ".png";
+
+  // ADD INSTRUMENT NAME AS CLASS ON THE BOX
   audio.src = "assets/" + instrument + ".mp3";
 
   box.append(img);
@@ -30,5 +33,21 @@ window.addEventListener("keyup", (e) => {
     const audio = document.createElement("audio");
     audio.src = "assets/" + instruments[e.key] + ".mp3";
     audio.play();
+
+    // FIND BOX WITH THE KEY
+    // APPLY BACKGROUND COLOR ON IT
+
+    const boxToApplyBackground = Array.from(
+      document.querySelectorAll(".box")
+    ).find((box) => {
+      return box.classList.contains(instruments[e.key]);
+    });
+
+    boxToApplyBackground.style.backgroundColor = "blanchedalmond";
+    setTimeout(() => {
+      boxToApplyBackground.style.backgroundColor = "white";
+    }, 500);
   }
 });
+
+
