59148e1dd3e35a42d111c3a1f38ecd41d4db000d
rohitjainfsl <rohitjainwebdev@gmail.com>
2024-03-20 15:45:34 +0530

    filterInTable done without css

---
+++ filterInTable/data.js
@@ -0,0 +1,91 @@
+let data = [
+  {
+    "id": 1,
+    "name": "Alice",
+    "age": 30,
+    "email": "alice@example.com",
+    "salary": 30000,
+    "order": 2,
+    "totalSepet": 23000,
+    "gender": "male",
+    "lastSeen": "10-11-2015"
+  },
+  {
+    "id": 2,
+    "name": "Bob",
+    "age": 25,
+    "email": "bob@example.com",
+    "salary": 32300,
+    "order": 5,
+    "totalSepet": 2300,
+    "gender": "male",
+    "lastSeen": "11-10-2017"
+  },
+  {
+    "id": 3,
+    "name": "Charlie",
+    "age": 28,
+    "email": "charlie@example.com",
+    "salary": 2000,
+    "order": 6,
+    "totalSepet": 23300,
+    "gender": "male",
+    "lastSeen": "09-02-2005"
+  },
+  {
+    "id": 4,
+    "name": "David",
+    "age": 35,
+    "email": "david@example.com",
+    "salary": 3030,
+    "order": 9,
+    "totalSepet": 200,
+    "gender": "male",
+    "lastSeen": "07-09-2018"
+  },
+  {
+    "id": 5,
+    "name": "Eva",
+    "age": 22,
+    "email": "eva@example.com",
+    "salary": 9000,
+    "order": 0,
+    "totalSepet": 2500,
+    "gender": "female",
+    "lastSeen": "10-01-2018"
+  },
+  {
+    "id": 6,
+    "name": "Frank",
+    "age": 31,
+    "email": "frank@example.com",
+    "salary": 30123,
+    "order": 1,
+    "totalSepet": 23000,
+    "gender": "male",
+    "lastSeen": "09-08-2011"
+  },
+  {
+    "id": 7,
+    "name": "Grace",
+    "age": 27,
+    "email": "grace@example.com",
+    "salary": 6000,
+    "order": 6,
+    "totalSepet": 3000,
+    "gender": "female",
+    "lastSeen": "12-11-2016"
+  },
+  {
+    "id": 8,
+    "name": "Henry",
+    "age": 29,
+    "email": "henry@example.com",
+    "salary": 12000,
+    "order": 4,
+    "totalSepet": 2300,
+    "gender": "male",
+    "lastSeen": "04-11-2011"
+  }
+]
+export default data
