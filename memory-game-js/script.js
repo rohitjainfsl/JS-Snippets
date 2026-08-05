e2159503278e35bd88028283c5a491c0dc60e719
rohitjainfslm <rohit@fullstacklearning.com>
2026-08-05 17:02:08 +0530

    Slight modifications in css of counter

--- memory-game-js/script.js
+++ memory-game-js/script.js
@@ -4,12 +4,15 @@ const startTimerDiv = document.querySelector("#startTimer");
 let openPhotos = 0;
 let firstClickPhoto = "";
 let secondClickPhoto = "";
+let timerInterval = "";
+let gameFinished = false;
+let busy = false;
 
 start.addEventListener("click", () => {
   document.querySelector("#start").style.display = "none";
   document.querySelector("#game").style.display = "flex";
   document.querySelector("#startTimer").style.display = "flex";
-  document.querySelector("#clickCounter").style.display = "block";
+  document.querySelector("#clickCounter").style.display = "flex";
   randomizePhotos();
 
   //START THE TIMER
@@ -21,54 +24,61 @@ start.addEventListener("click", () => {
 
 flip_card.forEach((card) => {
   card.onclick = (e) => {
+    if (gameFinished || busy) {
+      return;
+    }
+
     const card_inner = card.children[0];
     const styles = window.getComputedStyle(card_inner);
     if (styles.getPropertyValue("transform") === "none") {
       card_inner.style.transform = "rotateY(180deg)";
       openPhotos++;
 
-      if (openPhotos) {
-        setTimeout(() => {
-          if (openPhotos === 1) {
-            firstClickPhoto = card_inner.children[1].children[0];
-          } else if (openPhotos === 2) {
-            secondClickPhoto = card_inner.children[1].children[0];
-            if (firstClickPhoto.src === secondClickPhoto.src) {
-              firstClickPhoto.classList.add("matched");
-              secondClickPhoto.classList.add("matched");
-              // 2 cards were clicked & we found a match,
-              // now reset everything
-              firstClickPhoto = "";
-              secondClickPhoto = "";
-              openPhotos = 0;
-            } else {
-              // 2 cards were clicked & we didn't found a match,
-              // now reset everything & close the cards
-              firstClickPhoto = "";
-              secondClickPhoto = "";
-              openPhotos = 0;
-              closeAll();
-            }
+      if (openPhotos === 1) {
+        firstClickPhoto = card_inner.children[1].children[0];
+      } else if (openPhotos === 2) {
+        busy = true;
+        secondClickPhoto = card_inner.children[1].children[0];
+
+        if (firstClickPhoto.src === secondClickPhoto.src) {
+          firstClickPhoto.classList.add("matched");
+          secondClickPhoto.classList.add("matched");
+          // 2 cards were clicked & we found a match,
+          // now reset everything
+          firstClickPhoto = "";
+          secondClickPhoto = "";
+          openPhotos = 0;
+          busy = false;
+
+          if (allPhotosFlipped()) {
+            setTimeout(finishGame, 0);
           }
-        }, 1000);
+        } else {
+          // 2 cards were clicked & we didn't found a match,
+          // now reset everything & close the cards
+          setTimeout(() => {
+            firstClickPhoto = "";
+            secondClickPhoto = "";
+            openPhotos = 0;
+            closeAll();
+          }, 1000);
+        }
       }
     }
   };
 });
 
 function closeAll() {
-  setTimeout(() => {
-    openPhotos = 0;
-    flip_card.forEach((card) => {
-      if (
-        card.children[0].children[1].children[0].classList.contains(
-          "matched"
-        ) === false
-      ) {
-        card.children[0].removeAttribute("style");
-      }
-    });
-  }, 400);
+  flip_card.forEach((card) => {
+    if (
+      card.children[0].children[1].children[0].classList.contains(
+        "matched"
+      ) === false
+    ) {
+      card.children[0].removeAttribute("style");
+    }
+  });
+  busy = false;
 }
 
 function photos_match() {
@@ -87,36 +97,34 @@ function randomizePhotos() {
     "shahrukh.jpg",
     "tom cruise.jpg",
   ];
-  const alreadyUsedPhotos = [];
-  const alreadyUsedCards = [];
-  let j = 0;
-  let k = 0;
 
-  while (j < photos.length) {
-    const randomPhoto = photos[recursive(photos, alreadyUsedPhotos)];
-    for (let i = 0; i < 2; i++) {
-      flip_card[
-        recursive(flip_card, alreadyUsedCards)
-      ].children[0].children[1].children[0].src = "images/" + randomPhoto;
-    }
-    j++;
-  }
+  const randomizedDeck = shufflePhotos([...photos, ...photos]);
+
+  flip_card.forEach((card, index) => {
+    card.children[0].children[1].children[0].src =
+      "images/" + randomizedDeck[index];
+  });
 }
 
-function recursive(element, arr) {
-  const integer = Math.floor(Math.random() * element.length);
-  if (arr.includes(integer)) {
-    return recursive(element, arr);
-  } else {
-    arr.push(integer);
-    return integer;
+function shufflePhotos(photos) {
+  for (let shuffleCount = 0; shuffleCount < 3; shuffleCount++) {
+    for (let i = photos.length - 1; i > 0; i--) {
+      const randomIndex = getRandomNumber(i + 1);
+      [photos[i], photos[randomIndex]] = [photos[randomIndex], photos[i]];
+    }
   }
+
+  return photos;
+}
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
 }
 
 function startTimer() {
-  const interval = setInterval(() => {
+  timerInterval = setInterval(() => {
     if (Number(startTimerDiv.innerHTML) === 1) {
-      clearInterval(interval);
+      clearInterval(timerInterval);
       gameOver();
     } else startTimerDiv.innerHTML = Number(startTimerDiv.innerHTML) - 1;
   }, 1000);
@@ -125,7 +133,7 @@ function startTimer() {
 function startClickCounter() {
   const gameBoard = document.querySelector("#game-board");
   const clickCount = document.querySelector("#clickCounter span");
-  console.log(gameBoard.getBoundingClientRect());
+  // console.log(gameBoard.getBoundingClientRect());
   const gameBoardCoord = gameBoard.getBoundingClientRect();
 
   //if(user click is inside the game board)
@@ -139,7 +147,7 @@ function startClickCounter() {
       e.clientY > gameBoardCoord.top &&
       e.clientY < gameBoardCoord.bottom
     ) {
-      console.log("andar click hua hai");
+      // console.log("andar click hua hai");
       clickCount.innerHTML = Number(clickCount.innerHTML) + 1;
     }
   });
@@ -148,4 +156,34 @@ function startClickCounter() {
 function gameOver() {
   document.querySelector("#game").style.display = "none";
   document.querySelector("#startTimer").style.display = "none";
+}
+
+function allPhotosFlipped() {
+  return document.querySelectorAll(".matched").length === flip_card.length;
+}
+
+function finishGame() {
+  gameFinished = true;
+  clearInterval(timerInterval);
+
+  const clickCount = document.querySelector("#clickCounter span").innerHTML;
+  const timeRemaining = startTimerDiv.innerHTML;
+  const gameOverScreen = document.querySelector("#over");
+  const score = document.querySelector("#score");
+
+  gameOverScreen.querySelector("h1").innerHTML = "YOU WON!";
+  score.innerHTML = `Clicks: ${clickCount}<br />Time Remaining: ${timeRemaining}`;
+  gameOverScreen.style.display = "flex";
+  gameOverScreen.style.position = "fixed";
+  gameOverScreen.style.inset = "0";
+  gameOverScreen.style.zIndex = "999";
+  gameOverScreen.style.background = "rgba(0, 0, 0, 0.9)";
+  gameOverScreen.style.color = "#fff";
+  gameOverScreen.style.justifyContent = "center";
+  gameOverScreen.style.alignItems = "center";
+  gameOverScreen.style.flexDirection = "column";
+  gameOverScreen.style.textAlign = "center";
+  gameOverScreen.style.fontSize = "3rem";
+  gameOverScreen.style.fontWeight = "900";
+  score.style.marginTop = "1rem";
 }
